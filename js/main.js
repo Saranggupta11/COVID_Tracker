e1e9c9e48c0c8663cb01743a7feed0b85e8633ef
saranggupta11 <sarang.gupta0811@gmail.com>
2021-05-25 17:50:51 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,242 @@
+// function to fetch world data
+var fetchedData;
+var indiaData;
+function getData() {
+
+    fetch("https://covid-19-tracking.p.rapidapi.com/v1", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "30783a336emsh8f107ac4963783dp12931fjsn8dbc83f14b62",
+            "x-rapidapi-host": "covid-19-tracking.p.rapidapi.com"
+        }
+    })
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+            displayData(data);
+           countryData(data);
+
+        })
+}
+function displayData(data) {
+    let activeCase = document.getElementById('activecase')
+    let newcase = document.getElementById('newcase')
+    let newdeath = document.getElementById('newdeath')
+    let totalcase = document.getElementById('totalcase')
+    let totaldeath = document.getElementById('totaldeath')
+    let totalrecovered = document.getElementById('totalrecovered')
+    activeCase.innerHTML = data[0]['Active Cases_text']
+    newcase.innerHTML = data[0]['New Cases_text']
+    newdeath.innerHTML = data[0]['New Deaths_text']
+    totalcase.innerHTML = data[0]['Total Cases_text']
+    totaldeath.innerHTML = data[0]['Total Deaths_text']
+    totalrecovered.innerHTML = data[0]['Total Recovered_text']
+}
+getData()
+// onclick to display the data 
+function countryData(data) {
+    fetchedData=data;
+    let cont = document.getElementById('countryname')
+    let activeCase = document.getElementById('countryactive')
+    let newcase = document.getElementById('countrynewc')
+    let newdeath = document.getElementById('countrynewd')
+    let totalcase = document.getElementById('countrytotalc')
+    let totaldeath = document.getElementById('countrytotald')
+    let totalrecovered = document.getElementById('countrytotalr')
+    let alertcard=document.getElementById('cardalert')
+    let alerttext=document.getElementById('alerttext')
+    cont.innerHTML ='USA'
+    totalcase.innerHTML = "Total Cases: "+data[1]['Total Cases_text']
+    activeCase.innerHTML ="Active Cases: "+ data[1]['Active Cases_text']
+    totalrecovered.innerHTML ="Total Recovered: "+ data[1]['Total Recovered_text']
+    totaldeath.innerHTML = "Total Deaths: "+data[1]['Total Deaths_text']
+    if (data[1]['New Cases_text']==""){
+        data[1]['New Cases_text']='NA';
+    }
+    if (data[1]['New Deaths_text']=="") {
+        data[1]['New Deaths_text']='NA'
+    }
+    newcase.innerHTML = "New Cases: "+data[1]['New Cases_text']
+    newdeath.innerHTML = "New Deaths: "+data[1]['New Deaths_text']
+    alertcard.style.backgroundColor='red';
+    alerttext.innerHTML='USA is in HIGH risk zone as it is among top 10 worst COVID-19 affected countries so,please STAY HOME STAY SAFE';
+
+                 
+}
+
+function displayCountryData() {
+    console.log(fetchedData)
+    let str = document.getElementById('countrytext').value
+            let final = str.toUpperCase();
+            console.log(final)
+            fetchedData.forEach(function (element) {
+                if (element['Country_text'].toUpperCase() == final) {
+                    let alertIndex=fetchedData.indexOf(element)
+                    let alertcard=document.getElementById('cardalert')
+                    let alerttext=document.getElementById('alerttext')
+                    let cont = document.getElementById('countryname')
+                    let activeCase = document.getElementById('countryactive')
+                    let newcase = document.getElementById('countrynewc')
+                    let newdeath = document.getElementById('countrynewd')
+                    let totalcase = document.getElementById('countrytotalc')
+                    let totaldeath = document.getElementById('countrytotald')
+                    let totalrecovered = document.getElementById('countrytotalr')
+                    cont.innerHTML =element['Country_text']
+                    totalcase.innerHTML = "Total Cases: "+element['Total Cases_text']
+                    activeCase.innerHTML ="Active Cases: "+ element['Active Cases_text']
+                    totalrecovered.innerHTML ="Total Recovered: "+ element['Total Recovered_text']
+                    totaldeath.innerHTML = "Total Deaths: "+element['Total Deaths_text']
+                    if (element['New Cases_text']==""){
+                        element['New Cases_text']='NA';
+                    }
+                    if (element['New Deaths_text']=="") {
+                        element['New Deaths_text']='NA'
+                    }
+                    newcase.innerHTML = "New Cases: "+element['New Cases_text']
+                    newdeath.innerHTML = "New Deaths: "+element['New Deaths_text']
+                    if (alertIndex<=11) {
+                        alertcard.style.backgroundColor='red';
+                        alerttext.innerHTML=element['Country_text'].toUpperCase()+' is in HIGH risk zone, as it is among top 10 worst COVID-19 affected countries so,please STAY HOME STAY SAFE';
+                        
+                    }
+                     else if(alertIndex<=50){
+                         alertcard.style.backgroundColor='orange'
+                         alerttext.innerHTML=element['Country_text'].toUpperCase()+' is in MODERATE risk zone, as it is among top 50 worst COVID-19 affected countries so,please STAY HOME STAY SAFE';
+                     }
+                     else{
+                         alertcard.style.backgroundColor='green'
+                         alerttext.innerHTML=element['Country_text'].toUpperCase()+' is in LOW risk zone,as it is not among top 50 worst COVID-19 affected countries so,please STAY HOME STAY SAFE';
+                     }
+                }
+                })
+
+
+}
+let a = document.getElementById('get')
+a.addEventListener('click', () => {
+    displayCountryData();
+})
+
+function getIndiaData() {
+    fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api_india", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "30783a336emsh8f107ac4963783dp12931fjsn8dbc83f14b62",
+            "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com"
+        }
+    }).then(response => {
+	return response.json();
+}).then((data) => {
+            console.log(data)
+            let arr=data['state_wise']
+            indiaData=arr;
+            console.log(arr)
+            let table=document.getElementById('indiantotaldata')
+            let rowtotal=table.insertRow(-1)
+            let total=rowtotal.insertCell(0)
+            let activetotal=rowtotal.insertCell(1)
+            let confirmedtotal=rowtotal.insertCell(2)
+            let Deathstotal=rowtotal.insertCell(3)
+            let nctotal=rowtotal.insertCell(4)
+            let ndtotal=rowtotal.insertCell(5)
+            let nrtotal=rowtotal.insertCell(6)
+            let Recoveredtotal=rowtotal.insertCell(7)
+            total.innerHTML='Total'
+            activetotal.innerHTML=data['total_values'].active
+            confirmedtotal.innerHTML=data['total_values'].confirmed
+            Deathstotal.innerHTML=data['total_values'].deaths
+            nctotal.innerHTML=data['total_values'].deltaconfirmed
+            ndtotal.innerHTML=data['total_values'].deltadeaths
+            nrtotal.innerHTML=data['total_values'].deltarecovered
+            Recoveredtotal.innerHTML=data['total_values'].recovered
+
+
+            for(let i in arr){
+                let table=document.getElementById('indiandata');
+                let row=table.insertRow(-1);
+                let state=row.insertCell(0)
+                let active=row.insertCell(1)
+                let confirmed=row.insertCell(2)
+                let Deaths=row.insertCell(3)
+                let nc=row.insertCell(4)
+                let nd=row.insertCell(5)
+                let nr=row.insertCell(6)
+                let Recovered=row.insertCell(7)
+                if(arr[i].state!='State Unassigned'){
+                state.innerHTML=arr[i].state
+                active.innerHTML=arr[i].active
+                confirmed.innerHTML=arr[i].confirmed
+                Deaths.innerHTML=arr[i].deaths
+                nc.innerHTML=arr[i].deltaconfirmed
+                nd.innerHTML=arr[i].deltadeaths
+                nr.innerHTML=arr[i].deltarecovered
+                Recovered.innerHTML=arr[i].recovered
+                }
+                // console.log(i)
+                // console.log(arr[i])
+            }
+        })
+    }
+getIndiaData();
+const successCallback=(position)=>{
+    console.log(position);
+    let latitude  = position.coords.latitude;
+    let  longitude = position.coords.longitude;
+    console.log(latitude)
+    console.log(longitude)
+    reverseGeocoding(latitude, longitude)
+}
+const errorCallback=(error)=>{
+    console.log(error);
+}
+let loca= document.getElementById('getlocation')
+loca.addEventListener('click', () => {
+    navigator.geolocation.getCurrentPosition(successCallback,errorCallback)
+})
+
+
+
+function reverseGeocoding(latitude, longitude) {
+  fetch(`http://api.positionstack.com/v1/reverse?access_key=2ee46b7a5cf6f35051b853a6cc025d2a&query=${latitude},${longitude}`)
+  .then( res => res.json())
+  .then(response => {
+      console.log("User's Location Info: ", response)
+      let locah1=document.getElementById('locationh1')
+      let locacont=document.getElementById('locationcont')
+      let lostate=document.getElementById('locationstate')
+      let locadist=document.getElementById('locationdist')
+      locah1.innerHTML='Your Current Location is: '
+      locacont.innerHTML='Country: '+response.data[0].country
+      lostate.innerHTML='State: '+response.data[0].region
+      locadist.innerHTML='District: '+response.data[0].county
+      locah1.style.opacity=1;
+      locacont.style.opacity=1;
+      lostate.style.opacity=1;
+      locadist.style.opacity=1;
+      let locabox=document.getElementById('locationcoviddata')
+      let locadata1=document.getElementById('locationcoviddata1')
+      let locadata2=document.getElementById('locationcoviddata2')
+      let locadata3=document.getElementById('locationcoviddata3')
+      let locadata4=document.getElementById('locationcoviddata4')
+      let locadata5=document.getElementById('locationcoviddata5')
+      let locadata6=document.getElementById('locationcoviddata6')
+      let locadata7=document.getElementById('locationcoviddata7')
+      let locadata8=document.getElementById('locationcoviddata8')
+      locabox.style.opacity=1;
+      locadata1.innerHTML=response.data[0].county
+      locadata2.innerHTML="Active Cases: "+indiaData[response.data[0].region].district[response.data[0].county].active
+      locadata3.innerHTML="Total Confirmed: "+indiaData[response.data[0].region].district[response.data[0].county].confirmed
+      locadata4.innerHTML="Total Deaths: "+indiaData[response.data[0].region].district[response.data[0].county].deceased
+      locadata5.innerHTML="Total Recovered: "+indiaData[response.data[0].region].district[response.data[0].county].recovered
+      locadata6.innerHTML="New Confirmed: "+indiaData[response.data[0].region].district[response.data[0].county].delta.confirmed
+      locadata7.innerHTML="New Deaths: "+indiaData[response.data[0].region].district[response.data[0].county].delta.deceased
+      locadata8.innerHTML="New Recovered: "+indiaData[response.data[0].region].district[response.data[0].county].delta.recovered
+
+     
+   })
+   }
+  
+
+
+
